980415d8012317c68966eeb8c4e7e89f1abf730f
Mookgol <138899392+Mookgol@users.noreply.github.com>
2024-02-08 10:44:41 +0200

    clean up

--- src/Components /Navbar.js
+++ src/Components /Navbar.js
@@ -7,10 +7,19 @@ export default function Navbar(){
         <ul>
             <li>
                 <a href="/Home">Home</a>
+            </li>
+            <li>
                 <a href="/About">About</a>
+            </li>
+            <li>
                 <a href="/Projects">Projects</a>
-                <a href="/Experience">Experience</a>
-                <a href="/Contact">Contact</a>
+                <li>
+                    <a href="/Experience">Experience</a>
+                </li>
+                <li>
+                    <a href="/Contact">Contact</a>
+                </li>
+
             </li>
         </ul>
     </nav>
