ed95e945d9bb3c8ea663a8c6856bcdb8bc80b9cb
Mookgol <138899392+Mookgol@users.noreply.github.com>
2024-02-08 10:46:32 +0200

    clean up 2

--- src/Components /Navbar.js
+++ src/Components /Navbar.js
@@ -13,14 +13,13 @@ export default function Navbar(){
             </li>
             <li>
                 <a href="/Projects">Projects</a>
+            </li>
                 <li>
                     <a href="/Experience">Experience</a>
                 </li>
                 <li>
                     <a href="/Contact">Contact</a>
                 </li>
-
-            </li>
         </ul>
     </nav>
     )
