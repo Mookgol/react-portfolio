c9d58f5b5a00c1b0e2844ce4fa44f32029229169
Mookgol <138899392+Mookgol@users.noreply.github.com>
2024-02-09 12:03:52 +0200

    aligning image and text on hero image

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,13 +6,16 @@ import hero1 from '../components/Assets/hero1.png';
 function Home() {
     return (
         <section id="profile">
-            <div className="section__text">
-                <p className="section__text__p1">Hello, I'm</p>
-                <h1 className="title">Mookgo Lebaka</h1>
-                <p> A passionate Front-end Developer and Digital Illustrator<br/>based in Cape Town, South Africa. </p>
-            </div>
-            <div>
-                <img src={hero1} alt="Hero" className="hero-image"/>
+            <div className="profile__container">
+                <div className="section__text">
+                    <p className="section__text__p1">Hello, I'm</p>
+                    <h1 className="title">Mookgo Lebaka</h1>
+                    <p> A passionate Front-end Developer and Digital Illustrator<br/>based in Cape Town, South Africa.
+                    </p>
+                </div>
+                <div className="image__container">
+                    <img src={hero1} alt="Hero" className="hero-image"/>
+                </div>
             </div>
         </section>
     );
