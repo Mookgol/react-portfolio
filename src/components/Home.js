5b701b5f3b4e72d242f6e67ea9c22fc773215ddd
Mookgol <138899392+Mookgol@users.noreply.github.com>
2024-02-12 13:49:05 +0200

    added social logos

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,42 @@
 import React from 'react';
 import './Home.css';
 import hero1 from '../components/Assets/hero1.png';
+import resumePdf from '../components/Assets/resume-example.pdf';
+import linkedinIcon from '../components/Assets/linkedin-icon.png';
+import behanceIcon from '../components/Assets/behance-icon.png';
+import githubIcon from '../components/Assets/github-icon.png';
 
 
 function Home() {
+    const handleDownloadCv = () => {
+        window.open(resumePdf);
+    };
+
+    const handleContactinfo = () => {
+        window.location.href = './#contact';
+    };
+
     return (
         <section id="profile">
             <div className="profile__container">
                 <div className="section__text">
-                    <p className="section__text__p1">Hello, I'm</p>
+                    <h1 className="section__text__p1">Hello, I'm</h1>
                     <h1 className="title">Mookgo Lebaka</h1>
-                    <p> A passionate Front-end Developer and Digital Illustrator<br/>based in Cape Town, South Africa.
-                    </p>
+                    <h1> A passionate Junior Software Developer and Digital Illustrator
+                        <br/>based in Cape Town, South Africa.
+                    </h1>
+                    <h4>I Design and code beautiful simple things.</h4>
+                    <button className="btn btn-color-2" onClick={handleDownloadCv}>
+                        Download CV
+                    </button>
+                    <button className="btn btn-color-1" onClick={handleContactinfo}>
+                        Contact Info
+                    </button>
+                    <div className="social-icons">
+                        <img src={linkedinIcon} alt="LinkedIn" width="50" className="linkedin-icon"/>
+                        <img src={behanceIcon} alt="BehanceIcon" width="50" className="behance-icon"/>
+                        <img src={githubIcon} alt="githubIcon" width="50" className="github-icon"/>
+                    </div>
                 </div>
                 <div className="image__container">
                     <img src={hero1} alt="Hero" className="hero-image"/>
